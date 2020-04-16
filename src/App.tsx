38aebf9836d7bf4b5666744f2b7035195d512b5d
Warren Uhrich <hello@warren.codes>
2020-04-16 13:33:30 -0600

    Add 'inventory' types.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Grid, GridRow, Button } from 'semantic-ui-react';
+// import { connect } from 'react-redux';
+import { Grid } from 'semantic-ui-react';
 
 function App() {
   return (
     <Grid centered>
       <Grid.Row>
         <h1>Let's Try React &amp; Redux... Again!</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {/* Products. */}
+      </Grid.Row>
+      <Grid.Row>
+        {/* Cart. */}
       </Grid.Row>
     </Grid>
   );
